ccec98e2863efa1c476b727626209ed19380802c
Joel Besada <joel.besada@gmail.com>
2012-06-27 23:39:11 +0200

    settings...

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,8 +6,6 @@ env = ENV["RAILS_ENV"] || "development"
 # documentation.
 worker_processes 4
 
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
 listen 5000
 
 # Preload our app for more speed
@@ -19,47 +17,13 @@ timeout 30
 pid "/tmp/unicorn.pasteboard.me.pid"
 
 # Production specific settings
-if env == "production"
-  # Help ensure your application will always spawn in the symlinked
-  # "current" directory that Capistrano sets up.
-  working_directory "~/public/pasteboard.me/pasteboard/current"
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+working_directory "~/public/pasteboard.me/pasteboard/current"
 
-  # feel free to point this anywhere accessible on the filesystem
-  user 'zevas'
-  shared_path = "~/public/pasteboard.me/pasteboard/shared"
+# feel free to point this anywhere accessible on the filesystem
+user 'zevas'
+shared_path = "~/public/pasteboard.me/pasteboard/shared"
 
-  #stderr_path "#{shared_path}/log/unicorn.stderr.log"
-  #stdout_path "#{shared_path}/log/unicorn.stdout.log"
-end
-
-before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
-  old_pid = "/tmp/unicorn.pasteboard.me.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
-
-after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true",
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
-end
+#stderr_path "#{shared_path}/log/unicorn.stderr.log"
+#stdout_path "#{shared_path}/log/unicorn.stdout.log"
