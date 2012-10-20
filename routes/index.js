28958fab899b23bbb26cddfbc545a0c312ab784a
Joel Besada <joel.besada@gmail.com>
2012-10-20 23:23:03 +0200

    Convert file names to base62 strings

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,87 @@
 var knox = require("knox"),
-	formidable = require("formidable"),
-	microtime = require("microtime"),
-	easyimage = require("easyimage"),
-	request = require('request'),
-	fs = require('fs'),
-	auth = require('../auth'),
+  formidable = require("formidable"),
+  microtime = require("microtime"),
+  easyimage = require("easyimage"),
+  request = require('request'),
+  fs = require('fs'),
+  auth = require('../auth'),
   options = require("../options"),
-	knoxClient = null,
-	FILE_SIZE_LIMIT = 10 * 1024 * 1024; // 10 MB
+  knoxClient = null,
+  FILE_SIZE_LIMIT = 10 * 1024 * 1024, // 10 MB
+  BASE62_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 if (auth.amazon) {
-	knoxClient = knox.createClient({
-			key: auth.amazon.S3_KEY,
-			secret: auth.amazon.S3_SECRET,
-			bucket: auth.amazon.S3_BUCKET
-		});
+  knoxClient = knox.createClient({
+      key: auth.amazon.S3_KEY,
+      secret: auth.amazon.S3_SECRET,
+      bucket: auth.amazon.S3_BUCKET
+    });
 } else {
   console.log("Missing Amazon S3 credentials (/auth/amazon.js)");
 }
 
+/* Generates a unique file name with the given file type.
+   This current method generates names that are guaranteed
+   to be unique for 115 days (10^13 microseconds).
+ */
+function generateFileName(type) {
+  var fileExt = "." + (type === "image/jpeg" ? "jpg" : type.replace("image/", "")),
+    timeString = "" + microtime.now();
+  
+  timeString = timeString.substr(timeString.length - 13); // 13 last digits
+
+  return base62Encode(parseInt(timeString, 10)) + fileExt;
+}
+
+/* Converts an integer from base 10 to 62 */
+function base62Encode(n) {
+  var arr = [], r;
+
+  if (n === 0) return BASE62_CHARS[0];
+  
+  while (n) {
+    r = n % 62;
+    n = (n - r) / 62;
+    arr.push(BASE62_CHARS[r]);
+  }
+
+  return arr.reverse().join("");
+}
+
 /* GET, home page */
 exports.index =  function(req, res) {
-	var params = {
-		port: req.app.get("port"),
-		redirected: false,
-		useAnalytics: false,
-		trackingCode: ""
-	};
+  var params = {
+    port: req.app.get("port"),
+    redirected: false,
+    useAnalytics: false,
+    trackingCode: ""
+  };
 
-	if (!req.app.get('localrun') && auth.google_analytics) {
-		params.useAnalytics = true;
-		params.trackingCode = req.app.settings.env === "development" ? auth.google_analytics.development : auth.google_analytics.production;
-	}
-	if (req.cookies.redirected) {
-		params.redirected = true;
-		res.clearCookie('redirected');
-	}
-	res.render("index", params);
+  if (!req.app.get('localrun') && auth.google_analytics) {
+    params.useAnalytics = true;
+    params.trackingCode = req.app.settings.env === "development" ? auth.google_analytics.development : auth.google_analytics.production;
+  }
+  if (req.cookies.redirected) {
+    params.redirected = true;
+    res.clearCookie('redirected');
+  }
+  res.render("index", params);
 };
 
 /* GET, handle redirects from pasteshack */
 exports.redirected = function(req, res) {
-	res.cookie("redirected", true);
-	res.redirect('/');
+  res.cookie("redirected", true);
+  res.redirect('/');
 };
 
 /* GET, force an image to download */
 exports.download = function(req, res) {
   var imgReq;
-	if(knoxClient) {
-		imgReq = request("http://" + auth.amazon.S3_BUCKET + ".s3.amazonaws.com/" + auth.amazon.S3_IMAGE_FOLDER + req.params.image);
-	} else {
+  if(knoxClient) {
+    imgReq = request("http://" + auth.amazon.S3_BUCKET + ".s3.amazonaws.com/" + auth.amazon.S3_IMAGE_FOLDER + req.params.image);
+  } else {
     imgReq = request("http://" + req.headers.host + options.LOCAL_STORAGE_URL + req.params.image);
-	}
+  }
   res.set("Content-Disposition", "attachment");
   imgReq.pipe(res);
 };
@@ -70,23 +99,23 @@ exports.imageproxy = function(req, res) {
 exports.image = function(req, res) {
   var imageURL;
   if(knoxClient) {
-		imageURL = "http://" + auth.amazon.S3_BUCKET + ".s3.amazonaws.com/" + auth.amazon.S3_IMAGE_FOLDER + req.params.image;
+    imageURL = "http://" + auth.amazon.S3_BUCKET + ".s3.amazonaws.com/" + auth.amazon.S3_IMAGE_FOLDER + req.params.image;
   } else {
     imageURL = "http://" + req.headers.host + options.LOCAL_STORAGE_URL + req.params.image;
   }
     
-	var params = {
-		imageName: req.params.image,
-		imageURL: imageURL,
-		useAnalytics: false,
-		trackingCode: ""
-	};
+  var params = {
+    imageName: req.params.image,
+    imageURL: imageURL,
+    useAnalytics: false,
+    trackingCode: ""
+  };
 
-	if (!req.app.get('localrun') && auth.google_analytics) {
-		params.useAnalytics = true;
-		params.trackingCode = req.app.settings.env === "development" ? auth.google_analytics.development : auth.google_analytics.production;
-	}
-	res.render("image", params);
+  if (!req.app.get('localrun') && auth.google_analytics) {
+    params.useAnalytics = true;
+    params.trackingCode = req.app.settings.env === "development" ? auth.google_analytics.development : auth.google_analytics.production;
+  }
+  res.render("image", params);
 };
 
 /* GET, returns the short url for the given file name.
@@ -96,74 +125,74 @@ exports.image = function(req, res) {
  * TODO: Set correct status codes
  */
 exports.shorturl = function(req, res) {
-	if (!auth.parse) {
-		res.send("Missing Parse.com credentials", 500);
-		return;
-	}
-	if (!req.params.image) {
-		res.send("Missing image parameter", 500);
-		return;
-	}
-	query = encodeURIComponent('{"fileName":"' + req.params.image + '"}');
-	request({
-		method: "GET",
-		uri: "https://api.parse.com/1/classes/short_url?where=" + query,
-		headers: {
-			"X-Parse-Application-Id": auth.parse.APP_ID,
-			"X-Parse-REST-API-Key": auth.parse.API_KEY
-		}
-	}, function(error, response, body) {
-		if(!error && response.statusCode === 200) {
-			var result = JSON.parse(body).results[0];
-			if (result) {
-				res.json({url: result.shortURL});
-				return;
-			}
-		}
-		res.send("Not found", 500);
-	});
+  if (!auth.parse) {
+    res.send("Missing Parse.com credentials", 500);
+    return;
+  }
+  if (!req.params.image) {
+    res.send("Missing image parameter", 500);
+    return;
+  }
+  query = encodeURIComponent('{"fileName":"' + req.params.image + '"}');
+  request({
+    method: "GET",
+    uri: "https://api.parse.com/1/classes/short_url?where=" + query,
+    headers: {
+      "X-Parse-Application-Id": auth.parse.APP_ID,
+      "X-Parse-REST-API-Key": auth.parse.API_KEY
+    }
+  }, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var result = JSON.parse(body).results[0];
+      if (result) {
+        res.json({url: result.shortURL});
+        return;
+      }
+    }
+    res.send("Not found", 500);
+  });
 };
 
 /* POST, preuploads an image and stores it in /tmp */
 exports.preupload = function(req, res) {
-	var form = new formidable.IncomingForm(),
-		incomingFiles = [];
-	
-	form.parse(req, function(err, fields, files) {
-		var client = req.app.get("clients")[fields.id];
-		if (client) {
-			if (client.file) {
-				// Remove the old file
-				fs.unlink(client.file.path);
-			}
-			// Keep track of the current pre-uploaded file
-			client.file = files.file;
-		}
-		res.send("Received file");
+  var form = new formidable.IncomingForm(),
+    incomingFiles = [];
+  
+  form.parse(req, function(err, fields, files) {
+    var client = req.app.get("clients")[fields.id];
+    if (client) {
+      if (client.file) {
+        // Remove the old file
+        fs.unlink(client.file.path);
+      }
+      // Keep track of the current pre-uploaded file
+      client.file = files.file;
+    }
+    res.send("Received file");
 
-	});
-	form.on("fileBegin", function(name, file) {
-		incomingFiles.push(file);
-	});
-	form.on("aborted", function() {
-		// Remove temporary files that were in the process of uploading
-		for (var i = 0; i < incomingFiles.length; i++) {
-			fs.unlink(incomingFiles[i].path);
-		}
-	});
+  });
+  form.on("fileBegin", function(name, file) {
+    incomingFiles.push(file);
+  });
+  form.on("aborted", function() {
+    // Remove temporary files that were in the process of uploading
+    for (var i = 0; i < incomingFiles.length; i++) {
+      fs.unlink(incomingFiles[i].path);
+    }
+  });
 };
 
 /* POST, removes a preuploaded file from the given client ID */
 exports.clearfile = function(req, res) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-		var client = req.app.get("clients")[fields.id];
-		if (client && client.file) {
-			fs.unlink(client.file.path);
-			client.file = null;
-		}
-		res.send("Cleared");
-	});
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    var client = req.app.get("clients")[fields.id];
+    if (client && client.file) {
+      fs.unlink(client.file.path);
+      client.file = null;
+    }
+    res.send("Cleared");
+  });
 };
 
 /* POST, uploads a file to Amazon S3.
@@ -178,7 +207,6 @@ exports.upload = function(req, res) {
     var client,
       file,
       fileType,
-      fileExt,
       fileName,
       cropPath,
       uploadToAmazon,
@@ -207,9 +235,7 @@ exports.upload = function(req, res) {
       }
 
       fileType = file.type;
-      fileExt = fileType.replace("image/", "");
-      // Use microtime to generate a unique file name
-      fileName = microtime.now() + "." + (fileExt === "jpeg" ? "jpg" : fileExt);
+      fileName = generateFileName(fileType.replace("image/", ""));
       longURL = (req.app.get("localrun") ? "http://" + req.headers.host : req.app.get("domain")) + "/" + fileName;
 
 
